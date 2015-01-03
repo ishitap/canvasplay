28cc76b8c27cdcfb7bd1e3c0df5d4f08824f6f81
Ishita Prasad <ishitap@stanford.edu>
2015-01-03 09:17:10 -0800

    one clean commit

--- scripts/paperplay2.js
+++ scripts/paperplay2.js
@@ -1,6 +1,7 @@
 /* adapted from http://perfectionkills.com/exploring-canvas-drawing-techniques/ */
 
 tool.fixedDistance = 30;
+var pixels = [];
 
 function drawPixels(x, y) {
   for (var i = -15; i < 15; i+= 4) {
@@ -29,7 +30,42 @@ function onMouseMove(event) {
   drawPixels(event.middlePoint.x, event.middlePoint.y);
 }
 
-// el.onmousemove = function(e) {
-//   drawPixels(e.clientX, e.clientY);
-//   lastPoint = { x: e.clientX, y: e.clientY };
-// };
+//----
+
+function Particle ()
+{
+  this.scale = 1.0;
+  this.x = 0;
+  this.y = 0;
+  this.radius = 20;
+  this.color = "#000";
+  this.velocityX = 0;
+  this.velocityY = 0;
+  //this.scaleSpeed = 0.5;
+
+  this.update = function(ms)
+  {
+    // shrinking
+    //this.scale -= this.scaleSpeed * ms / 1000.0;
+
+    if (this.scale <= 0)
+    {
+      this.scale = 0;
+    }
+    // moving away from explosion center
+    this.x += this.velocityX * ms/1000.0;
+    this.y += this.velocityY * ms/1000.0;
+  };
+
+  this.draw = function(context2D)
+  {
+    // translating the 2D context to the particle coordinates
+    context2D.save();
+    context2D.translate(this.x, this.y);
+
+    context2D.fillStyle = this.color;
+    context2D.fillRect(this.x, this.y, 4, 4);
+
+    context2D.restore();
+  };
+}
